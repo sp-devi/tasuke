d85a80c151332939335be6c5fae8ab4849f3553f
spdevi <spneuro01@gmail.com>
2021-05-26 12:45:43 +0900

    Fix poll command for tasuke bot

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 import os
 
-from slack_bolt import App
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt import App, Ack
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
 )
 
+def clean_emoji(emoji):
+    return emoji.replace(":", "").replace('"', "").strip()
+
+@app.message("tsk poll")
+def ask_who(message, say, client, logger):
+    try:
+        logger.info(message)
+        outerGroup = re.compile("\((.+)\)")
+        pollMsgMatch = outerGroup.search(message["text"])
+        pollMsg = pollMsgMatch.group(1)
+
+        msgBody = pollMsg.split(",")
+
+        channel_id = message["channel"]
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text="*{}* _poll created by {}._".format(msgBody[0], message['user'])
+        )
+
+        channel_id = response["channel"]
+        ts = response["ts"]
+
+        for i in range(1, len(msgBody)):
+            client.reactions_add(
+                channel=channel_id, name=clean_emoji(msgBody[i]), timestamp=ts
+            )
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
-
-
-@app.message("knock2")
-def ask_who(message, say):
-    print("kxx")
-    try:
-        say(f"Hey there <@{message['user']}>!")
-    except Exception as e:
-        say(f"Error publishing home tab: {e}")
-
-
-@app.message("knock")
-def ask_who_knocked(client, message):
-    channel_id = message["channel"]
-    client.chat_scheduleMessage(channel=channel_id, post_at=1601510399, text="Go")
-
-@app.event("app_home_opened")
-def update_home_tab(client, event, say):
-    say("Hello, I'm Tasuke.")
